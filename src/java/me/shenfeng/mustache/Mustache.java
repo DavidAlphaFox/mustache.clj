a3a63f71559a888d5c1511bd6942e0949024ad28
David Gao <david.alpha.fox@gmail.com>
2025-06-28 15:37:46 +0800

    mustache scanner

--- src/java/me/shenfeng/mustache/Mustache.java
+++ src/java/me/shenfeng/mustache/Mustache.java
@@ -16,15 +16,16 @@ public class Mustache {
     public static final String END = "}}";
 
     List<Token> tokens;
-
+  //内嵌缓存
     public static final ConcurrentMap<String, Mustache> CACHE = new ConcurrentHashMap<String, Mustache>();
-
+  //在转义内部的内嵌模式
+  //对section进行进一步的处理，得到更细致的的token列表
     private List<Token> nestedToken(List<Token> input) throws ParserException {
         List<Token> output = new ArrayList<Token>();
         Deque<Token> sections = new LinkedList<Token>(); // stack
         Token section;
-        List<Token> collector = output;
-
+        List<Token> collector = output; // 字串收集，最后的返回结果
+        //遍历Token列表
         for (Token token : input) {
             switch (token.type) {
             case Token.INVERTED:
@@ -39,26 +40,30 @@ public class Mustache {
                 if (sections.isEmpty()) {
                     throw new ParserException("Unopened section: " + token.value);
                 }
+                //得到最后一个Section
                 section = sections.removeLast();
+                //名字不同
                 if (!section.value.equals(token.value)) {
                     throw new ParserException("Unclosed " + token.value + "; in section"
                             + section.value);
                 }
-
                 if (sections.size() > 0) {
+                  //如果还有sessions没闭合
+                  //那么就将内容收集器，设置为最后一个sections
                     collector = sections.peekLast().tokens;
                 } else {
                     collector = output;
                 }
                 break;
             default:
-                collector.add(token);
+              collector.add(token); //如果是普通字串，直接加入结果中
                 break;
             }
         }
         if (sections.size() > 0) {
             throw new ParserException("Unclosed section: " + sections.peek().value);
         }
+        //返回最后的结果
         return output;
     }
 
@@ -75,22 +80,33 @@ public class Mustache {
         List<Token> tokens = new LinkedList<Token>();
         Scanner scanner = new Scanner(template);
         while (!scanner.eos()) {
+          //去寻找mustache的开头字串
             String value = scanner.scanUtil(BEGIN);
-            if (value != null)
-                tokens.add(new Token(Token.TEXT, value));
+            //截取字串有值
+            if (value != null) {
+              //将字串放入token表
+              tokens.add(new Token(Token.TEXT, value));
+            }
+            //向下读取类型
             char type = scanner.nextType();
+            //跳过所有空格
             scanner.skipeWhiteSpace();
             switch (type) {
-            case '{': // not escape
+              case '{': // not escape
+                //是{{{，说明不是转义开头
+                //向后扫描，直到发现}}}为止
                 value = scanner.scanUtil("}}}");
                 tokens.add(new Token(type, value));
                 break;
-            default:
+              default:
+                //否则向前寻找}}
+                //把这个字串错位name保存起来
                 value = scanner.scanUtil(END);
                 tokens.add(new Token(type, value));
             }
 
         }
+        //当所有的tokens都处理完成了，则对tokens内部进行处理
         this.tokens = nestedToken(tokens);
     }
 
--- src/java/me/shenfeng/mustache/Scanner.java
+++ src/java/me/shenfeng/mustache/Scanner.java
@@ -14,23 +14,32 @@ public class Scanner {
     public boolean eos() { // end of string
         return idx == template.length() - 1;
     }
-
+  //向前扫描直到遇到对应的模式
     public String scanUtil(String patten) {
+      //从idx处遍历模板
         int pos = template.indexOf(patten, idx);
         if (pos == -1) {
+          //并未找到对应的模式，将从idx处剩余字串切割出去
             String match = template.substring(idx);
+            // idx回退一个位置
             idx = template.length() - 1;
+            // 返回剩余的字串
             return match;
         } else if (pos == 0) {
+          //正好在idx处发现模式，那么直接跳过模式的长度
             idx += patten.length();
+            //返回null
             return null;
         } else {
+          //剪切当前位置道模式前的子串
             String match = template.substring(idx, pos);
+            // 将位置调整为模式后的第一个字符
             idx = pos + patten.length();
+            //返回字串
             return match;
         }
     }
-
+  //向前移动直到第一个非空，或者到字串结尾了
     public void skipeWhiteSpace() {
         while (Character.isWhitespace(next()) && !eos()) {
             ++idx;
@@ -40,24 +49,24 @@ public class Scanner {
     private char next() {
         return template.charAt(idx);
     }
-
+  //直接回退count个字节
     public void pushBack(int count) {
         idx -= count;
     }
-
+  //得到下个Char
     public char nextType() {
         skipeWhiteSpace();
         if (!eos()) {
-            char c = next();
-            if (TAGS.indexOf(c) != -1) {
-                idx += 1;
-                if (c == Token.TRUE) {
-                    System.err.println("WARN: use nonstandard ?");
-                }
-                return c;
-            } else {
-                return Token.NAME;
+          char c = next(); //下一个字符
+          if (TAGS.indexOf(c) != -1) { //是否是TAGS
+            idx += 1; //是TAGS，就继续
+            if (c == Token.TRUE) {
+              System.err.println("WARN: use nonstandard ?");
             }
+            return c;
+          } else {
+            return Token.NAME;
+          }
         }
         return 0;
     }
