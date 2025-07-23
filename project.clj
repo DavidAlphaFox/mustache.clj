8122bbd0e81f841d7bf005acb8b67185a6b5065f
David Gao <david.alpha.fox@gmail.com>
2025-07-23 23:46:06 +0800

    update version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject me.shenfeng/mustache "1.2-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "Mustache write in java, for clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]]
   :warn-on-reflection true
   :java-source-paths ["src/java"]
   :jar-exclusions [#".*java$"]
-  :javac-options ["-source" "1.6" "-target" "1.6" "-g" "-encoding" "utf8"]
+  :javac-options ["-source" "21" "-target" "21" "-g" "-encoding" "utf8"]
   :plugins [[lein-swank "1.4.4"]]
   :profiles {:dev {:dependencies [[junit/junit "4.8.2"]
                                   [stencil "0.3.2"]
